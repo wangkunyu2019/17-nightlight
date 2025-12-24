6ab817c436d7a44ade2eda35e6f744a03f6737ff
wangkunyu2019 <2019040262@qq.com>
2025-12-24 21:46:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 basic.forever(function () {
-	
+    if (input.lightLevel() > 100) {
+        basic.clearScreen()
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
